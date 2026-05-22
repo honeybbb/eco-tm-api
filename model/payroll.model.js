973ca7df06d5933f7613a834157403caa12016b2
danbi <danbi681591@gmail.com>
2026-05-22 17:15:03 +0900

    05/22 - 급여 기본 정보에서 장애여부 가져오기

--- model/payroll.model.js
+++ model/payroll.model.js
@@ -67,7 +67,7 @@ exports.getBaseSalary = async function (cIdx) {
     sql += " c.itemNm as role,";
     sql += " c.sort,";
     sql += " m.name as staff,";
-    sql += " m.disability,"
+    sql += " m.disability,m.disability_grade, m.disability_date,"
     //자동 계산 여부
     sql += " IFNULL(mbs.isAutoCalc, IFNULL(mc.isAutoCalc, 'Y')) as isAutoCalc,";
 
